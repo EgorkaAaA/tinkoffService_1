2a8363c60d4fd1e12110ca35bdc6ac58bc0acbfd
Лебедев Егор Игоревич <eilebedev@phoenixit.ru>
2022-01-11 18:38:17 +0300

    add telegram bot

---
+++ TelegramBot/src/main/java/tbot/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,15 @@
+package tbot.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
+
+}
